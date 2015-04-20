370266def29cd96b0082ee8246ed49c2098527e0
Maciej Małecki <me@mmalecki.com>
2015-04-20 13:56:50 -0700

    Implement the SSH runner

---
+++ index.js
@@ -0,0 +1,36 @@
+var assert = require('assert')
+var child_process = require('child_process')
+var shellQuote = require('shell-quote')
+
+module.exports = function(context) {
+  var target = context.args.ssh || context.env.SSH
+  assert(target || typeof target !== 'string', '--ssh [<user>]@<host> is required')
+
+  function exec(cmd, options, cb) {
+    assert(!'implement me!')
+  }
+
+  function spawn(cmd, args, options) {
+    if (args && !Array.isArray(args) && !options) {
+      options = args
+      args = []
+    }
+
+    if (!args) args = []
+
+    var sshArgs = ['-tt'] // force tty allocation
+
+    if (!context.args.strictHostKeyChecking && !context.env.STRICT_HOST_KEY_CHECKING)
+      sshArgs.push('-o', 'StrictHostKeyChecking=no')
+
+    sshArgs.push(target, shellQuote.quote([cmd].concat(args)))
+
+    var ssh = child_process.spawn('ssh', sshArgs, options)
+    return ssh
+  }
+
+  return {
+    exec: exec,
+    spawn: spawn,
+  }
+}
